43128b20932606ff767a296e43812e056785b1b0
Antonio Furtado <antoniordf@hotmail.com>
2023-01-28 23:24:04 +0000

    Refactor of constructor function to set state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {manager: ''};
-  }
+  // This "state" variable is equivalent to doing "constructor(props) { super(props); this.state = {manager: ''};}"
+  state = {
+    manager: ''
+  };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
